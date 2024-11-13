05aaf7b29ba288e4092955b263294db5d998f7bc
Marshall331 <ahmad31100@live.fr>
2024-11-13 20:36:01 +0100

    y

--- src/Controller/TestTwigController.php
+++ src/Controller/TestTwigController.php
@@ -13,30 +13,35 @@ class TestTwigController extends AbstractController
     {
         $users = [];
 
-        $user = ['full_name' => 'Nonne', 'age' => 25];
+        $user = ['createdAt' => $this->getRandomDate(), 'full_name' => 'Nonne', 'age' => 25, 'image' => rand(1, 500)];
         $users[] = $user;
 
-        $user = ['full_name' => 'Pelleau', 'age' => 75];
+        $user = ['createdAt' => $this->getRandomDate(), 'full_name' => 'Pelleau', 'age' => 75, 'image' => rand(1, 500)];
         $users[] = $user;
 
-        $user = ['full_name' => 'Chanouha', 'age' => 124];
+        $user = ['createdAt' => $this->getRandomDate(), 'full_name' => 'Chanouha', 'age' => 124, 'image' => rand(1, 500)];
         $users[] = $user;
 
-        $user = ['full_name' => 'Bruel', 'age' => 15];
+        $user = ['createdAt' => $this->getRandomDate(), 'full_name' => 'Bruel', 'age' => 15, 'image' => rand(1, 500)];
         $users[] = $user;
 
-        $user = ['full_name' => 'Teste', 'age' => 8];
+        $user = ['createdAt' => $this->getRandomDate(), 'full_name' => 'Teste', 'age' => 8, 'image' => rand(1, 500)];
         $users[] = $user;
 
-        $user = ['full_name' => 'Stolf', 'age' => 28];
+        $user = ['createdAt' => $this->getRandomDate(), 'full_name' => 'Stolf', 'age' => 28, 'image' => rand(1, 500)];
         $users[] = $user;
 
-        $user = ['full_name' => 'Sotin', 'age' => 5];
+        $user = ['createdAt' => $this->getRandomDate(), 'full_name' => 'Sotin', 'age' => 5, 'image' => rand(1, 500)];
         $users[] = $user;
 
         return $this->render('test_twig/index.html.twig', [
             'title' => 'page accueil',
             'users' => $users
         ]);
+    }
+
+    private function getRandomDate(): String
+    {
+        return  rand(1900, 2025) . '/' . rand(1, 12) . '/' . rand(1, 29);
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 60, nullable: true)]
+    private ?string $full_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    /**
+     * @var Collection<int, Address>
+     */
+    #[ORM\OneToMany(targetEntity: Address::class, mappedBy: 'user')]
+    private Collection $addresses;
+
+    public function __construct()
+    {
+        $this->addresses = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->full_name;
+    }
+
+    public function setFullName(?string $full_name): static
+    {
+        $this->full_name = $full_name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Address>
+     */
+    public function getAddresses(): Collection
+    {
+        return $this->addresses;
+    }
+
+    public function addAddress(Address $address): static
+    {
+        if (!$this->addresses->contains($address)) {
+            $this->addresses->add($address);
+            $address->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAddress(Address $address): static
+    {
+        if ($this->addresses->removeElement($address)) {
+            // set the owning side to null (unless already changed)
+            if ($address->getUser() === $this) {
+                $address->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+}
