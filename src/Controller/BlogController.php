45263858cf9e375e6bdf55a066f159599032fb6d
Marshall331 <ahmad31100@live.fr>
2024-12-06 15:53:30 +0100

    fin crud articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,100 +3,83 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
-use PhpParser\Node\Expr\Cast\String_;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/blog')]
-class BlogController extends AbstractController
+final class BlogController extends AbstractController
 {
-    #[Route('/hello', name: 'app_hello')]
-    public function hello(): Response
-    {
-        return new Response('Hello');
-    }
-
-    #[Route('/{id}/{nom}', name: 'app_blog_indexOld', requirements: ['id' => '\d+', 'nom' => '[a-zA-Z]{3,15}'])]
-    public function indexOld($id, $nom): Response
-    {
-        return $this->render(
-            'blog/indexOld.html.twig',
-            [
-                'id' => $id,
-                'nom' => $nom,
-            ]
-        );
-    }
-
-    #[Route('/{slug}', name: 'app_blog_show_slug', methods: ['GET'])]
-    public function showSlug(ArticleRepository $articleRepository, String $slug): Response
-    {
-        return $this->render(
-            'blog/show.html.twig',
-            [
-                'article' => $articleRepository->findOneBySlug($slug)
-            ]
-        );
-    }
-
-    // #[Route('/{id}', name: 'app_blog_show', methods: ['GET'])]
-    // public function show(Article $article): Response
-    // {
-    //     return $this->render(
-    //         'blog/show.html.twig',
-    //         [
-    //             'article' => $article
-    //         ]
-    //     );
-    // }
-
     #[Route(name: 'app_blog_index', methods: ['GET'])]
-    public function index(ArticleRepository  $articleRepository): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
-        return $this->render(
-            'blog/index.html.twig',
-            [
-                'articles' => $articleRepository->findAll()
-            ]
-        );
+        return $this->render('blog/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
     }
 
     #[Route('/new', name: 'app_blog_new', methods: ['GET', 'POST'])]
-    public function new($id, $nom): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render(
-            'blog/indexOld.html.twig',
-            [
-                'id' => $id,
-                'nom' => $nom,
-            ]
-        );
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setCreatedAt(new \DateTimeImmutable());
+
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_blog_show', ['slug' => $article->getSlug()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('blog/new.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'app_blog_show', methods: ['GET'])]
+    public function show(Article $article): Response
+    {
+        return $this->render('blog/show.html.twig', [
+            'article' => $article,
+        ]);
     }
 
     #[Route('/{id}/edit', name: 'app_blog_edit', methods: ['GET', 'POST'])]
-    public function edit($id, $nom): Response
+    public function edit(Request $request, Article $article, EntityManagerInterface $entityManager): Response
     {
-        return $this->render(
-            'blog/indexOld.html.twig',
-            [
-                'id' => $id,
-                'nom' => $nom,
-            ]
-        );
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setUpdatedAt(new \DateTimeImmutable());
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_blog_show', ['slug' => $article->getSlug()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('blog/edit.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
     }
 
     #[Route('/{id}', name: 'app_blog_delete', methods: ['POST'])]
-    public function delete($id, $nom): Response
+    public function delete(Request $request, Article $article, EntityManagerInterface $entityManager): Response
     {
-        return $this->render(
-            'blog/indexOld.html.twig',
-            [
-                'id' => $id,
-                'nom' => $nom,
-            ]
-        );
+        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
     }
 }
