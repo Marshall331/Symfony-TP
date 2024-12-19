ea8ba0d9691c475d27603677ca047f585e0d8d83
AhmadK <ahmad31100@live.fr>
2024-12-19 14:36:03 +0100

    update route

--- src/Controller/AthleteController.php
+++ src/Controller/AthleteController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/athlete')]
+#[Route('/JO/athlete')]
 final class AthleteController extends AbstractController
 {
     #[Route(name: 'app_athlete_index', methods: ['GET'])]
