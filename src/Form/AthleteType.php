d127f1183d45e42079d0260fe4e4b24593381a4f
AhmadK <ahmad31100@live.fr>
2024-12-19 21:49:06 +0100

    amélioration twig

--- src/Form/AthleteType.php
+++ src/Form/AthleteType.php
@@ -7,25 +7,57 @@ use App\Entity\Discipline;
 use App\Entity\Pays;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class AthleteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomPrenom')
-            ->add('dateNaiss', null, [
-                'widget' => 'single_text',
+            ->add('nomPrenom', TextType::class, options: [
+                'label' => 'Nom & prénom :',
+                'attr' => ['class' => 'form-control mt-2'],
             ])
-            ->add('photo')
-            ->add('genre')
-            ->add('taille')
-            ->add('poids')
+            ->add('dateNaiss', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de naissance :',
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => ['class' => 'form-control mb-2'],
+                'required' => false,
+                'data' => new \DateTimeImmutable(),
+            ])
+            ->add('photo', TextType::class, options: [
+                'label' => 'Photo :',
+                'attr' => ['class' => 'form-control mt-2'],
+            ])
+            ->add('genre', ChoiceType::class, [
+                'label' => 'Genre :',
+                'label_attr' => ['class' => 'form-label'],
+                'choices' => [
+                    'Masculin' => 'M',
+                    'Féminim' => 'F',
+                ],
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('taille', NumberType::class, options: [
+                'label' => 'Taille :',
+                'attr' => ['class' => 'form-control mt-2'],
+            ])
+            ->add('poids', NumberType::class, options: [
+                'label' => 'Poids :',
+                'attr' => ['class' => 'form-control mt-2'],
+            ])
             ->add('pays', EntityType::class, [
                 'class' => Pays::class,
-                'choice_label' => 'id',
+                'label' => 'Pays :',
+                'choice_label' => 'nomPays',
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => ['class' => 'form-control'],
             ])
             ->add('discipline', EntityType::class, [
                 'class' => Discipline::class,
